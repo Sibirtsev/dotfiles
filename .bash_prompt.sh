429021e740d4476b2cba682871ca16c596e77e4b
Alexey Sibirtsev <alexey.sibirtsev+github@gmail.com>
2018-09-12 18:56:12 +0200

    Update .bash_prompt.sh

--- .bash_prompt.sh
+++ .bash_prompt.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-#
-# DESCRIPTION:
-#
-#   Set the bash prompt according to:
-#    * the active virtualenv
-#    * the branch/status of the current git repository
-#    * the return value of the previous command
-#    * the fact you just came from Windows and are used to having newlines in
-#      your prompts.
-#
-# USAGE:
-#
-#   1. Save this file as ~/.bash_prompt
-#   2. Add the following line to the end of your ~/.bashrc or ~/.bash_profile:
-#        . ~/.bash_prompt
-#
-# LINEAGE:
-#
-#   Based on work by woods
-#
-#   https://gist.github.com/31967
 
 # The various escape codes that we can use to color our prompt.
         RED="\[\033[0;31m\]"
@@ -31,6 +10,14 @@ YELLOW_DARK="\[\033[0;33m\]"
 LIGHT_GREEN="\[\033[1;32m\]"
       WHITE="\[\033[1;37m\]"
  LIGHT_GRAY="\[\033[0;37m\]"
+ COLOR_VENV="\[\033[0;46;37m\]"
+ COLOR_USER="\[\033[0;47;32m\]"
+ COLOR_AT="\[\033[0;47;95m\]"
+ COLOR_IP="\[\033[0;47;36m\]"
+ COLOR_PATH="\[\033[5;47;34m\]"
+ COLOR_BRANCH_GREEN="\[\033[7;100;32m\]"
+ COLOR_BRANCH_YELLOW="\[\033[7;100;33m\]"
+ COLOR_BRANCH_RED="\[\033[7;47;91m\]"
  COLOR_NONE="\[\e[0m\]"
 
 # Detect whether the current directory is a git repository.
@@ -44,12 +31,12 @@ function set_git_branch {
   git_status="$(git status 2> /dev/null)"
 
   # Set color based on clean/staged/dirty.
-  if [[ ${git_status} =~ "working directory clean" ]]; then
-    state="${GREEN}"
-  elif [[ ${git_status} =~ "Changes to be committed" ]]; then
-    state="${YELLOW}"
+  if [[ ${git_status} =~ "othing to commit" ]]; then
+    state="${COLOR_BRANCH_GREEN}"
+  elif [[ ${git_status} =~ "hanges to be committed" ]]; then
+    state="${COLOR_BRANCH_YELLOW}"
   else
-    state="${LIGHT_RED}"
+    state="${COLOR_BRANCH_RED}"
   fi
 
   # Set arrow icon based on status against remote.
@@ -95,12 +82,12 @@ function set_virtualenv () {
   if test -z "$VIRTUAL_ENV" ; then
       PYTHON_VIRTUALENV=""
   else
-      PYTHON_VIRTUALENV="${BLUE}[`basename \"$VIRTUAL_ENV\"`]${COLOR_NONE} "
+      PYTHON_VIRTUALENV="${COLOR_VENV}[`basename \"$VIRTUAL_ENV\"`]${COLOR_NONE} "
   fi
 }
 
 function set_ip () {
-  THEIP=$(ifconfig | grep "inet " | grep -v 127.0.0. | awk '{print $2}')
+  THEIP=$(ifconfig | grep "inet " | grep -v 127.0.0. | awk '{print $2}' | sed -n -e 'H;${x;s/\n/,/g;s/^,//;p;}')
 }
 
 # Set the full bash prompt.
@@ -123,10 +110,9 @@ function set_bash_prompt () {
 
   # Set the bash prompt variable.
   PS1="
-${PYTHON_VIRTUALENV}${GREEN}\u@${THEIP} ${YELLOW_DARK}\w${COLOR_NONE} ${BRANCH}
+${PYTHON_VIRTUALENV}${COLOR_USER}\u${COLOR_AT}@${COLOR_IP}${THEIP}:${COLOR_PATH}\w ${COLOR_NONE}${BRANCH}
 ${PROMPT_SYMBOL} "
 }
 
 # Tell bash to execute this function just before displaying its prompt.
 PROMPT_COMMAND=set_bash_prompt
-
